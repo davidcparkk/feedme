7f867c1781e9aac82c362f88095b936eca93f303
David Park <xdavidpark@gmail.com>
2019-03-03 02:14:16 -0800

    Adjust nav bar

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import logo from '../../../app/assets/images/feedly-512.png';
+import logo from '/home/david/Documents/App Academy/Projects/Feedme/feedMe/app/assets/images/feedly.svg';
 
 const Greeting = (props) => {
   const signinLink = () => {
     return (
       <nav className = 'login-signup'>
-        <header>
-          <h1>Feedme</h1>
-        </header>
+        <div className="feedme-welcome-container">
+            <img src={require('/home/david/Documents/App Academy/Projects/Feedme/feedMe/app/assets/images/feedly.svg')} alt="" className='icon-img' size={24} />
+            
+          </div>
         <div >
         <Link to='/' className='login-button'>Home</Link>
         <Link to='/login' className='login-button'>Login</Link>
@@ -26,10 +27,15 @@ const Greeting = (props) => {
     
     return(
       <div className='header'>
-        {/* <img src={logo} alt=""/> */}
-        <p>Start</p>
-        <h2 className='header-name'>Hi, {props.currentUser.username}!</h2>
-        <button className='header-button' onClick={handleLogout}>Log Out</button>
+        <div className="feedme-welcome-container">
+            <img src={require('/home/david/Documents/App Academy/Projects/Feedme/feedMe/app/assets/images/feedly.svg')} alt="" className='icon-img' size={24} />
+            <div className='feedme-welcome'>feedme</div>
+          </div>
+        
+        <div className='profile-container'>
+          <h2 className='header-name'>Hi, {props.currentUser.username}</h2>
+          <button className='header-button' onClick={handleLogout}>Log Out</button>
+        </div>
       </div>
     )
   }
