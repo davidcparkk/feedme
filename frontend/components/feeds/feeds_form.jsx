03e0d3b97ce203465387a108a7f6b0c0c8bf9417
David Park <xdavidpark@gmail.com>
2019-03-04 14:09:36 -0800

    Upload updated create feed form

--- frontend/components/feeds/feeds_form.jsx
+++ frontend/components/feeds/feeds_form.jsx
@@ -9,6 +9,11 @@ class FeedsForm extends React.Component {
       feed_title: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.goBack = this.goBack.bind(this);
+  }
+
+  goBack() {
+    this.props.history.goBack();
   }
 
   update(field) {
@@ -25,20 +30,29 @@ class FeedsForm extends React.Component {
 
   render () {
     return (
-      <div className='display-container'>
-        <h3>Create </h3>
-        <form onSubmit={this.handleSubmit}>
-          <div onClick={this.props.closeModal} className='close-x'>X</div>
-            <label>Title
-              <input
-                type="text"
-                value={this.state.feed_title}
-                onChange={this.update('feed_title')} />
-            </label>
-
-            <input type="submit" value={this.props.formType} />
-          
-        </form>
+      <div>
+        <div onClick={this.props.closeModal} className='close-x'>X</div>
+        <div className='create-new-feed-container'>
+          <h1 className = 'news-feed-header'>Create New Feed</h1>
+          <h3 className='news-feed-description'>A private collection of trusted sources, keyword alerts, or twitter feeds you want to read</h3>
+          <div className='separator-small'></div>
+          <form onSubmit={this.handleSubmit} className='create-feed-form'>
+              <div >
+              <label>Title
+                <p className='required-tag'>Required</p>
+                <input  className='create-feed-input'
+                  type="text"
+                  value={this.state.feed_title}
+                  onChange={this.update('feed_title')} placeholder="Title" />
+              </label>
+              </div>
+              <div className='separator-small'></div>
+              <div className='separator-small'></div>
+              <input type="submit" value="CREATE" className="create-feed-button" />
+              <button className="create-feed-cancel" onClick={this.props.closeModal}>CANCEL</button>
+            
+          </form>
+        </div>
       </div>
     );
   }
